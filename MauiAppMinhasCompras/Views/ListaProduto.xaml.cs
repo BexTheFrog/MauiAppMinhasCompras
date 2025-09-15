e88fd79b0306925cf27500821871509468833655
bexthefrog <beatriz.go.plateiro@gmail.com>
2025-09-15 05:43:55 -0300

    Alterações na Model

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -21,7 +21,6 @@ public partial class ListaProduto : ContentPage
         try
         {
             lista.Clear();
-
             List<Produto> tmp = await App.Db.GetAll();
        
             tmp.ForEach(i => lista.Add(i));
