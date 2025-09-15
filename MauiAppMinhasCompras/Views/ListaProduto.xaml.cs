3de702670cec00eaaf2d223db92510bb4a7b4fc0
bexthefrog <beatriz.go.plateiro@gmail.com>
2025-09-15 01:57:40 -0300

    Atualização da classe produtos, visualização de produtos gravados no banco, criado a funcionalidade de pesquisa, formatado valores para exibição na tela;

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,11 +1,25 @@
+using System.Collections.ObjectModel;
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
-	public ListaProduto()
-	{
-		InitializeComponent();
-	}
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+    public ListaProduto()
+    {
+        InitializeComponent();
+
+        lst_produtos.ItemsSource = lista;
+    }
+
+    protected async override void OnAppearing()
+    {
+        List<Produto> tmp = await App.Db.GetAll();
+
+        lista.Clear();
+        tmp.ForEach(i => lista.Add(i));
+    }
 
     private void Adicionar_Produto(object sender, EventArgs e)
     {
@@ -18,5 +32,25 @@ public partial class ListaProduto : ContentPage
         {
             DisplayAlert("Ops, erro ao abrir a página ", erro.Message, "Fechar");
         }
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void Somar_Clicked(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+
+        string msg = $"O total é {soma:C}";
+
+        DisplayAlert("Total dos produtos", msg,"Ok");
     }
 }
